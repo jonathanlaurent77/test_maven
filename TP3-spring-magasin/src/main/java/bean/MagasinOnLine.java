7d8d97a357f47a987580bcd4c9e79bfdf2656bf9
jonathanlaurent77 <jonathanlaurent77@gmail.com>
2022-10-27 10:26:51 +0200

    first commit

---
+++ TP3-spring-magasin/src/main/java/bean/MagasinOnLine.java
@@ -0,0 +1,20 @@
+package bean;
+
+public class MagasinOnLine {
+	private String nom;
+
+	public MagasinOnLine(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
